ee888cfd75ee6df887ea69796942952b5e1cfc7a
cjdiniz <cjsdiniz@outlook.com>
2021-07-29 23:48:16 +0100

    Hello World

--- hello.py
+++ hello.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # return 'hello world'
-    return render_template('home.html')
+    return 'hello world'
+    # return render_template('home.html')
 
 
 if __name__ == "__main__":
